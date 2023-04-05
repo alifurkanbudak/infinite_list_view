b059901c1daadbfad201c833c9f11e276e8f4279
Ali Furkan Budak <40214943+alifurkanbudak@users.noreply.github.com>
2023-04-05 02:39:51 -0500

    .

--- lib/infinite_list_view.dart
+++ lib/infinite_list_view.dart
@@ -3,4 +3,4 @@ library infinite_list_view;
 export 'src/infinite_list_view.dart';
 export 'src/infinite_loader.dart';
 export 'src/infinite_scroll_physics.dart';
-export 'src/visibility_callbacks.dart';
+export 'src/visibility_config.dart';
